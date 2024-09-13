3e10b1973bb6dfd1f4b09c463b769656bae7f0e1
brian <brianotina20@gmail.com>
2024-09-14 01:05:33 +0300

    all components added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
-const Hero = () => {
-    return <>
-         <section className="bg-indigo-700 py-20 mb-4">
+const Hero = ({title}) => {
+    return (
+        <section className="bg-indigo-700 py-20 mb-4">
             <div
             className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
             >
@@ -8,7 +8,7 @@ const Hero = () => {
                     <h1
                     className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
                     >
-                    Become a React Dev
+                    {title}
                     </h1>
                     <p className="my-4 text-xl text-white">
                     Find the React job that fits your skills and needs
@@ -16,8 +16,7 @@ const Hero = () => {
                 </div>
             </div>
         </section>
-
-    </>
+    )
 }
 
 export default Hero
